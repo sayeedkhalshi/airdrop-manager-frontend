e4cdc4b9e8fc38b93cc29fae976fb4d383ef7883
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2025-03-10 16:23:53 +0600

    basic structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { GoogleAnalytics } from "@next/third-parties/google";
+// import { GoogleAnalytics } from "@next/third-parties/google";
 import "@rainbow-me/rainbowkit/styles.css";
 import { ReduxProvider } from "@/redux/Provider";
 import "./globals.css";
@@ -11,42 +10,38 @@ import "./globals.css";
 import { RainbowProviders } from "@/lib/RainbowProvider";
 //import Footer from "@/components/Footer/Footer";
 
-
-
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Okabul",
-  description: "Manage Airdrops",
-};
+// export const metadata: Metadata = {
+//   title: "Okabul",
+//   description: "Manage Airdrops",
+// };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <RainbowProviders>
-                    <ReduxProvider>
-                        
-                        {children}
-                      
-                    </ReduxProvider>
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                <RainbowProviders>
+                    <ReduxProvider>{children}</ReduxProvider>
                 </RainbowProviders>
-                <GoogleAnalytics gaId="G-J9E9LGZ479" />
-      </body>
-    </html>
-  );
+                {
+                    //<GoogleAnalytics gaId="G-J9E9LGZ479" />
+                }
+            </body>
+        </html>
+    );
 }
